ea605749b89313e739fedf5aff44b85c4cea6ec9
Mike Rehberg <git@TR182.com>
2014-12-23 05:40:33 -0500

    Added The-Charge splash screen

--- example1/example1.ino
+++ example1/example1.ino
@@ -15,7 +15,13 @@ SoftwareSerial mySerial(3,2); // pin 2 = TX, pin 3 = RX (unused)
 void setup()
 {
   mySerial.begin(9600); // set up serial port for 9600 baud
-  delay(500); // wait for display to boot up
+  delay(2000); // wait for display to boot up
+//  mySerial.write(124);    // set display
+//  mySerial.write(3);      //     20 Characters
+//  mySerial.write(124);    // set display
+//  mySerial.write(5);      //     4 lines
+  
+  
 }
 
 void loop()
@@ -23,13 +29,15 @@ void loop()
   mySerial.write(254); // move cursor to beginning of first line
   mySerial.write(128);
 
-  mySerial.write("                "); // clear display
-  mySerial.write("                ");
+  mySerial.write("12345678901234567890"); // clear display
+  mySerial.write("1                  1");
+  mySerial.write("2   The-Charge     2");
+  mySerial.write("3  FRC Team 2619   3"); // clear display
 
   mySerial.write(254); // move cursor to beginning of first line
-  mySerial.write(128);
+  mySerial.write(192);
  
-  mySerial.write("Hello, world!");
+  //mySerial.write("1  Hello, World!   1");
 
   while(1); // wait forever
 }
