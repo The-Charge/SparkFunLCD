337253cfdcc655040839a05acd7d2f2b1a8aed07
Mike Rehberg <Git@TR182.com>
2014-12-23 06:18:53 -0500

    Added clearScreen function

--- example1/example1.ino
+++ example1/example1.ino
@@ -10,35 +10,40 @@
 #include <SoftwareSerial.h>
 
 // Attach the serial display's RX line to digital pin 2
-SoftwareSerial mySerial(3,2); // pin 2 = TX, pin 3 = RX (unused)
+SoftwareSerial LCD(3,2); // pin 2 = TX, pin 3 = RX (unused)
 
 void setup()
 {
-  mySerial.begin(9600); // set up serial port for 9600 baud
-  delay(2000); // wait for display to boot up
-//  mySerial.write(124);    // set display
-//  mySerial.write(3);      //     20 Characters
-//  mySerial.write(124);    // set display
-//  mySerial.write(5);      //     4 lines
-  
+  LCD.begin(9600);  // set up serial port for 9600 baud
+  delay(2000);      // wait for display to boot up
   
 }
 
 void loop()
 {
-  mySerial.write(254); // move cursor to beginning of first line
-  mySerial.write(128);
+  clearScreen();
+  
+  LCD.write(254); // move cursor to beginning of first line
+  LCD.write(128);
 
-  mySerial.write("12345678901234567890"); // clear display
-  mySerial.write("1                  1");
-  mySerial.write("2   The-Charge     2");
-  mySerial.write("3  FRC Team 2619   3"); // clear display
+  LCD.write("12345678901234567890"); // clear display
+  LCD.write("1                  1");
+  LCD.write("2   The-Charge     2");
+  LCD.write("3  FRC Team 2619   3"); // clear display
 
-  mySerial.write(254); // move cursor to beginning of first line
-  mySerial.write(192);
+  LCD.write(254); // move cursor to beginning of first line
+  LCD.write(192);
  
-  //mySerial.write("1  Hello, World!   1");
+  delay(2000);      // leave splash screen up for 2 sec.
+
+  LCD.write("1  Hello, World!   1");
 
   while(1); // wait forever
 }
 
+void clearScreen()
+{
+  //clears the screen, you will use this a lot!
+  LCD.write(0xFE);
+  LCD.write(0x01); 
+}
